21090592f938c7c5af97ffae8eac2a355695073a
myungwan-git <nsubway1070635@gmail.com>
2022-01-09 19:56:04 +0900

    20220109 while Complete

--- algorithm/arrayLess.c
+++ algorithm/arrayLess.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 int main() {
-    short count = 0;
-    short lessthan = 0;
-    scanf("%hd %hd", &count, &lessthan);
-    short arr[count];
+    int count = 0, standard = 0;
+    scanf("%d %d", &count, &standard);
+    int compareTargetArr[count];
+    int compareTargetResult[count];
+    int resultNumber = 0;
 
-    for(short i=0; i<count; i++) {
-        scanf("%hd",arr[i]);
-    }
     for(int i=0; i<count; i++) {
-        printf("%hd \n", arr[i]);
+        scanf("%d", &compareTargetArr[i]);
+        if(compareTargetArr[i] < standard) {
+            compareTargetResult[resultNumber] = compareTargetArr[i];
+            resultNumber++;
+        }
     }
+
+    for(int i=0; i<resultNumber; i++) {
+        printf("%d ",compareTargetResult[i]);
+    }
+    printf("\n");
 
     return 0;
 }
